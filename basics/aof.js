bcdcef314ffd3751eb82b4ed036827a663963e28
HARSHIT PORWAL <porwalharshit88@gmail.com>
2025-12-10 06:39:50 +0000

    object and fun

---
+++ basics/aof.js
@@ -0,0 +1,15 @@
+const obj={
+    name:"hp",
+    c:"btech"
+}
+
+let arr=[1,2,3,4];
+for(let i=0; i<4; i++){
+    console.log(arr[i]);
+}
+
+const fun=()=>{
+    console.log(5+5);
+}
+
+console.log([typeof(obj), typeof(arr), typeof(fun)]);
