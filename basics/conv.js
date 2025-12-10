f19f2f6bfef8be037d93f5863c53348d820bba71
HARSHIT PORWAL <porwalharshit88@gmail.com>
2025-12-10 06:16:15 +0000

    conversion

---
+++ basics/conv.js
@@ -0,0 +1,9 @@
+let num=33;
+
+let Snum=String(num);
+console.log("snum is:",Snum);
+console.table([typeof(num), typeof(Snum)]);
+
+let bnum=Boolean(num);
+console.log(bnum);
+console.log(typeof(bnum));
