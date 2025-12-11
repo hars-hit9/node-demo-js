3488c43f880cebc8ad5009fa84831aec5715ab60
HARSHIT PORWAL <porwalharshit88@gmail.com>
2025-12-11 07:05:52 +0000

    date

--- basics/date.js
+++ basics/date.js
@@ -4,3 +4,7 @@ console.log(d.toDateString());
 console.log(d.toLocaleDateString());
 
 console.log(d.toLocaleString());
+
+console.log(d.toLocaleString('defult', {month:'long'}));
+
+console.log(Date.now());
